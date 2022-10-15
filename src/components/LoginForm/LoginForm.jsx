ace1407ca68a37feaf907b66c597156852952d91
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-10-15 17:09:27 -0300

    Empezando el pop up de sign in

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./LoginForm.css";
+
+export const LoginForm = ({ isShowLogin }) => {
+  return (
+    <div className={`${!isShowLogin ? "active" : ""} show`}>
+      <div className="login-form">
+        <div className="form-box solid">
+          <form>
+            <h1 className="login-text">Sign In</h1>
+            <label>Email</label>
+            <br></br>
+            <input type="text" name="email" className="login-box" />
+            <br></br>
+            <label>Password</label>
+            <br></br>
+            <input type="password" name="password" className="login-box" />
+            <br></br>
+            <input type="submit" value="LOGIN" className="login-btn" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { LoginForm } from "../../components/LoginForm/LoginForm";
 import { Presentation } from "../../components/presentation/Presentation";
 
 export const Home = () => {
   return (
     <div>
       <Presentation />
+      <LoginForm />
     </div>
   );
 };
