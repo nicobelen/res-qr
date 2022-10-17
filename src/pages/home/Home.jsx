0e9187fa3ee8a36aa308278992918349a1d83a17
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-10-17 18:46:03 -0300

    login pop up terminado

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { LoginForm } from "../../components/LoginForm/LoginForm";
 import { Presentation } from "../../components/presentation/Presentation";
 
 export const Home = () => {
   return (
     <div>
       <Presentation />
-      <LoginForm />
     </div>
   );
 };
